94cacc5dbaaa7ebbfb1651cf70dfa75eeb034938
FU DoA <htg00713@163.com>
2018-08-26 16:28:17 +0800

    8月25日pm4:30

--- reg/views.py
+++ reg/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from reg.models import user
 from django.http import HttpResponse
-def index (request):
+
+
+def index(request):
     return render(request,'reg/reg.html')
 
-def check (request):
-    user_id_check=request.GET['reg_id']
-    if user.objects.filter(id=user_id_check)!=[]:
-        return  HttpResponse('100')
-    elif user.objects.filter(id=user_id_check)==[]:
-        return HttpResponse('200')
+
+def check(request):
+    user_id_check = request.GET.get('reg_id')
+    user_result = user.objects.filter(id=user_id_check)
+    if user_result:
+        return HttpResponse(100)
+    else:
+        return HttpResponse(200)
 
 
 def reg(request):
-    reg_id=request.POST.get('reg_id')
-    reg_salt =request.POST.get('reg_salt')
-    reg_pswmd5=request.POST.get('reg_pswmd5')
-    if user.objects.filter(id=user_id_check)!=[]:
-        return HttpResponse('400')
+    reg_id = request.POST.get('reg_id')
+    reg_salt = request.POST.get('reg_salt')
+    reg_pswmd5 = request.POST.get('reg_pswmd5')
+    user_result = user.objects.filter(id=reg_id)
+    if user_result:
+        return HttpResponse(400)
     else:
-        user.objects.create(id=reg_id,salt=reg_salt,pswmd5=reg_pswmd5)
-        return hHttpResponse('300')
+        user.objects.create(id=reg_id, salt=reg_salt, pswmd5=reg_pswmd5)
+        return HttpResponse(300)
--- testwork/urls.py
+++ testwork/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 from booklib import  views as booklib_views
 from fanyi import views as fanyi_views
 from main import views as main_views
